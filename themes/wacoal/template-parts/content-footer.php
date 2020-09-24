84817a485d80544cf3493f028418c7a70fe724f6
Prajna <prajna.mishra@wwindia.com>
2020-09-24 10:55:44 +0530

    added footer widgets

--- themes/wacoal/template-parts/content-footer.php
+++ themes/wacoal/template-parts/content-footer.php
@@ -1,3 +1,8 @@
 <footer class="content-footer">
     Footer Content Goes here
+    <div class=""><?php dynamic_sidebar('footer-1');?></div>
+    <div class=""><?php dynamic_sidebar('footer-2');?></div>
+    <div class=""><?php dynamic_sidebar('footer-2');?></div>
+    <div class=""><?php dynamic_sidebar('footer-4');?></div>
+    <div class=""><?php dynamic_sidebar('footer-5');?></div>
 </footer>
