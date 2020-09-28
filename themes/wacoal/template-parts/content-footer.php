489d6512b4c748ecd71d3274529a3630134c0369
Tushar F <tfirake@cemtrexlabs.com>
2020-09-28 10:11:58 +0530

    Feat - UI component first commit

--- themes/wacoal/template-parts/content-footer.php
+++ themes/wacoal/template-parts/content-footer.php
@@ -1,8 +1,142 @@
-<footer class="content-footer">
-    Footer Content Goes here
-    <div class=""><?php dynamic_sidebar('footer-1');?></div>
-    <div class=""><?php dynamic_sidebar('footer-2');?></div>
-    <div class=""><?php dynamic_sidebar('footer-2');?></div>
-    <div class=""><?php dynamic_sidebar('footer-4');?></div>
-    <div class=""><?php dynamic_sidebar('footer-5');?></div>
+<footer class="footer-section">
+    <div class="footer-wrapper">
+        <div class="footer-wrapper--left">
+            <div class="footer-links">
+                <div class="footer-links--title">
+                    Shop
+                </div>
+                <ul class="footer-links--ul">
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">New Arrivals</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Bras</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">dd+</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Plus Size</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Panties</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Shape Wear</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Lingerie</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Last Chance</a>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div class="footer-links">
+                <div class="footer-links--title">
+                    Wacoal America
+                </div>
+                <ul class="footer-links--ul">
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">About Us</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Email sign up</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Fit for the cure</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Wacoal stores</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Store Locator</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Wacoal Canada</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Careers</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="footer-links">
+                <div class="footer-links--title">
+                Customer Support
+                </div>
+                <ul class="footer-links--ul">
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Shipping & Return</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">FAQ</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Contact us</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">E-gift cards</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Order Status</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="footer-links">
+                <div class="footer-links--title">
+                    Bra Education
+                </div>
+                <ul class="footer-links--ul">
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Bra Finder</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Bra fir calculator</a>
+                    </li>
+                    <li class="footer-links--ul__list">
+                        <a href="" class="footer-links--ul__link">Fit Consultants</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-wrapper--right">
+            <div class="footer-images">
+                <img class="footer-images--img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/footer-img-1.png" alt="Wacoal Image 1" />
+                <img class="footer-images--img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/footer-img-2.png" alt="Wacoal Image 2" />
+                <img class="footer-images--img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/footer-img-3.png" alt="Wacoal Image 3" />
+                <img class="footer-images--img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/footer-img-4.png" alt="Wacoal Image 4" />
+            </div>
+
+            <div class="footer-social">
+                <a href="" class="footer-social--icon">
+                    <img src="<?php echo  get_theme_file_uri(); ?>/assets/images/facebook-icon.svg" alt="Wacoal Facebook" />
+                </a>
+                <a href="" class="footer-social--icon">
+                    <img src="<?php echo  get_theme_file_uri(); ?>/assets/images/twitter-icon.svg" alt="Wacoal Twitter" />
+                </a>
+                <a href="" class="footer-social--icon">
+                    <img src="<?php echo  get_theme_file_uri(); ?>/assets/images/youtube-icon.svg" alt="Wacoal Youtube" />
+                </a>
+                <a href="" class="footer-social--icon">
+                    <img src="<?php echo  get_theme_file_uri(); ?>/assets/images/instagram-icon.svg" alt="Wacoal Instagram" />
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-wrapper">
+        <div class="footer-wrapper--copyright">
+            ©2020 Wacoal. All Rights Reserved. Terms of Use. Privacy Policy. Site Map
+        </div>
+    </div>
 </footer>
+
+<!-- <div class=""><?php //dynamic_sidebar('footer-1');?></div>
+    <div class=""><?php //dynamic_sidebar('footer-2');?></div>
+    <div class=""><?php //dynamic_sidebar('footer-2');?></div>
+    <div class=""><?php //dynamic_sidebar('footer-4');?></div>
+    <div class=""><?php //dynamic_sidebar('footer-5');?></div> -->
--- themes/wacoal/template-parts/content-header.php
+++ themes/wacoal/template-parts/content-header.php
@@ -2,11 +2,11 @@
     <img class="header-section--logo" src="<?php echo  get_theme_file_uri(); ?>/assets/images/wacoal-logo.svg" alt="Wacoal" />
 </header>
 
-    <div class="header-navigation">
+    <nav class="header-navigation">
         <ul class="header-navigation--ul">
             <li class="header-navigation--list"><a href="" class="header-navigation--link">WACOAL 101</a></li>
             <li class="header-navigation--list"><a href="" class="header-navigation--link">STYLE GUIDE</a></li>
             <li class="header-navigation--list"><a href="" class="header-navigation--link">BRA TRENDS: TIPS & HOW TO’S</a></li>
             <li class="header-navigation--list"><a href="" class="header-navigation--link">BRA’DROBE</a></li>
         </ul>
-    </div>
+    </nav>
