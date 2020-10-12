8728fa130a8c166ba6b6c2f7d4144a8c2a8ca07d
vidyapawar95 <vpawar@cemtrexlabs.com>
2020-10-12 15:34:29 +0530

    Refactor
    - make image caption editor in wacoal image block.

--- themes/wacoal/assets/js/admin/acf-custom-text-toolbar.js
+++ themes/wacoal/assets/js/admin/acf-custom-text-toolbar.js
@@ -7,9 +7,10 @@
     //   qtInit.buttons = 'strong,em,del';
 
     // }
-    if ($field[0].id == 'paragraph_content' || $field[0].id == 'quote_text'
+    if ($field[0].id == 'paragraph_content' || $field[0].id == 'image_caption'
+        || $field[0].id == 'video_cap'
         || $field[0].id == 'description' || $field[0].id == 'list_description'
-        || $field[0].id == 'answer_text' || $field[0].id == 'video_cap' ) {
+        || $field[0].id == 'answer_text'  ) {
       qtInit.buttons = 'strong,em,del,link,ol,ul';
     }
 
