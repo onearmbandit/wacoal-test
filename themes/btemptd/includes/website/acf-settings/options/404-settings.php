5b0826651c86816fc1f06ae87a8b98e0a201d064
Prajna <prajna.mishra@wwindia.com>
2020-10-13 14:30:02 +0530

    feat acf settings added

--- themes/btemptd/includes/website/acf-settings/options/404-settings.php
+++ themes/btemptd/includes/website/acf-settings/options/404-settings.php
@@ -6,47 +6,47 @@
  */
 if( function_exists('acf_add_local_field_group') ):
 
-acf_add_local_field_group(array(
-	'key' => 'group_5f7ee4bf97fba',
-	'title' => '404 page settings',
-	'fields' => array(
-		array(
-			'key' => 'field_5f7ee4e496f20',
-			'label' => 'Short Description',
-			'name' => 'description',
-			'type' => 'wysiwyg',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'default_value' => '',
-			'tabs' => 'all',
-			'toolbar' => 'full',
-			'media_upload' => 1,
-			'delay' => 0,
-		),
-	),
-	'location' => array(
-		array(
-			array(
-				'param' => 'options_page',
-				'operator' => '==',
-				'value' => 'acf-options-404-page',
-			),
-		),
-	),
-	'menu_order' => 0,
-	'position' => 'normal',
-	'style' => 'default',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => '',
-	'active' => true,
-	'description' => '',
-));
+    acf_add_local_field_group(array(
+        'key' => 'group_5f8459ec0e630',
+        'title' => '404 page settings',
+        'fields' => array(
+            array(
+                'key' => 'field_5f8459f687354',
+                'label' => 'Short description',
+                'name' => 'short_description',
+                'type' => 'wysiwyg',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'tabs' => 'all',
+                'toolbar' => 'full',
+                'media_upload' => 1,
+                'delay' => 0,
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'acf-options-404-page',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
 
-endif;
+    endif;
--- themes/btemptd/includes/website/common/website-common-function.php
+++ themes/btemptd/includes/website/common/website-common-function.php
@@ -23,154 +23,6 @@ function btemptd_pingback_header()
 }
 add_action('wp_head', 'btemptd_pingback_header');
 /**
- * Add posted time to post listing
- *
- * @return void
- */
-
-if (!function_exists('btemptd_posted_on')) {
-
-    function btemptd_posted_on()
-    {
-        $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-        if (get_the_time('U') !== get_the_modified_time('U')) {
-            $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-        }
-
-        $time_string = sprintf(
-            $time_string,
-            esc_attr(get_the_date(DATE_W3C)),
-            esc_html(get_the_date()),
-            esc_attr(get_the_modified_date(DATE_W3C)),
-            esc_html(get_the_modified_date())
-        );
-
-        $posted_on = sprintf(
-            esc_html_x('Posted on %s', 'post date', 'btemptd'),
-            '<a href="' . esc_url(get_permalink()) . '" rel="bookmark">' . $time_string . '</a>'
-        );
-
-        echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore
-
-    }
-};
-/**
- * change Author html
- *
- * @return html
- */
-if (!function_exists('btemptd_posted_by')) {
-
-    function btemptd_posted_by()
-    {
-        $byline = sprintf(
-            esc_html_x('by %s', 'post author', 'btemptd'),
-            '<span class="author vcard"><a class="url fn n" href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . '</a></span>'
-        );
-
-        echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore
-
-    }
-};
-/**
- * change edit post link
- *
- * @return void
- */
-if (!function_exists('btemptd_entry_footer')) {
-
-    function btemptd_entry_footer()
-    {
-        // Hide category and tag text for pages.
-        if ('post' === get_post_type()) {
-
-            $categories_list = get_the_category_list(esc_html__(', ', 'btemptd'));
-            if ($categories_list) {
-                printf('<span class="cat-links">' . esc_html__('Posted in %1$s', 'btemptd') . '</span>', $categories_list); // phpcs:ignore
-            }
-
-            $tags_list = get_the_tag_list('', esc_html_x(', ', 'list item separator', 'btemptd'));
-            if ($tags_list) {
-                printf('<span class="tags-links">' . esc_html__('Tagged %1$s', 'btemptd') . '</span>', $tags_list); // phpcs:ignore
-            }
-        }
-
-        if (!is_single() && !post_password_required() && (comments_open() || get_comments_number())) {
-            echo '<span class="comments-link">';
-            comments_popup_link(
-                sprintf(
-                    wp_kses(
-                        __('Leave a Comment<span class="screen-reader-text"> on %s</span>', 'btemptd'),
-                        array(
-                            'span' => array(
-                                'class' => array(),
-                            ),
-                        )
-                    ),
-                    get_the_title()
-                )
-            );
-            echo '</span>';
-        }
-
-        edit_post_link(
-            sprintf(
-                wp_kses(
-                    __('Edit <span class="screen-reader-text">%s</span>', 'btemptd'),
-                    array(
-                        'span' => array(
-                            'class' => array(),
-                        ),
-                    )
-                ),
-                get_the_title()
-            ),
-            '<span class="edit-link">',
-            '</span>'
-        );
-    }
-};
-/**
- * change html of post thumbnail
- *
- * @return html
- */
-if (!function_exists('btemptd_post_thumbnail')) {
-
-    function btemptd_post_thumbnail()
-    {
-        if (post_password_required() || is_attachment() || !has_post_thumbnail()) {
-            return;
-        }
-
-        if (is_singular()) :
-            ?>
-
-<div class="post-thumbnail">
-            <?php the_post_thumbnail(); ?>
-</div><!-- .post-thumbnail -->
-
-<?php else : ?>
-
-<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-    <?php
-        the_post_thumbnail(
-            'post-thumbnail', array(
-            'alt' => the_title_attribute(
-                array(
-                'echo' => false,
-                )
-            ),
-            )
-        );
-    ?>
-</a>
-
-    <?php
-endif; // End is_singular().
-    }
-};
-/**
  * Get header html
  *
  * @param string $classname class name.
@@ -226,7 +78,6 @@ function btemptd_get_image_alt($attachmentId, $default = null)
     }
     return $image_alt;
 }
-
 /**
  * btemptd remove p tag from content
  *
@@ -239,7 +90,6 @@ function btemptd_remove_p_tag($content)
     $content = str_ireplace('</p>', '', $content);
     return $content;
 }
-
 /**
  * Add Menu link class.
  *
@@ -304,17 +154,7 @@ add_action('upload_mimes', 'btemptd_add_svg_file_types_to_uploads');
  */
 function btemptd_widgets_init()
 {
-    register_sidebar(
-        array(
-        'name' => esc_html__('Sidebar', 'btemptd'),
-        'id' => 'sidebar-1',
-        'description' => esc_html__('Add widgets here.', 'btemptd'),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget' => '</section>',
-        'before_title' => '<h2 class="widget-title">',
-        'after_title' => '</h2>',
-        )
-    );
+
     register_sidebar(
         array(
         'name'          => esc_html__('Footer Column One', 'btemptd'),
@@ -620,66 +460,3 @@ function btemptd_paging_nav()
     echo '</div></div></section>' . "\n";
 }
 
- /**
-  * Function to remove 2 recent posts from post listing
-  *
-  * @param  array $query wp_query array
-  * @return array $query wp_query array
-  */
-function btemptd_exclude_posts_from_specific_category( $query )
-{
-
-    if (is_admin() || ! $query->is_main_query() ) {
-        return;
-    }
-
-    if ($query->is_archive() ) {
-
-        $get_cat_ID=get_term_by('slug', $query->query_vars['category_name'], 'category');
-
-        $featured_posts = get_posts(
-            array(
-            'numberposts' => 2,
-            'cat' => $get_cat_ID->term_id,
-            'offset' => 0,
-            'orderby' => 'post_date',
-            'order' => 'DESC',
-            'post_status'=>'publish'
-            )
-        );
-        foreach( $featured_posts as $featured_post ) {
-            $posts_to_exclude[]    = $featured_post->ID;
-        }
-        $query->set('post__not_in', $posts_to_exclude);
-    }
-
-}
-add_action('pre_get_posts', 'btemptd_exclude_posts_from_specific_category');
-
-/**
- * Function to enable the taxonomies on pages
- *
- * @return void
- */
-function btemptd_add_taxonomies_to_pages()
-{
-    register_taxonomy_for_object_type('category', 'page');
-}
-add_action('init', 'btemptd_add_taxonomies_to_pages');
-
-function btemptd_block_categories( $categories, $post ) {
-    if ( $post->post_type !== 'post' ) {
-        return $categories;
-    }
-    return array_merge(
-        $categories,
-        array(
-            array(
-                'slug' => 'btemptd',
-                'title' => __( 'btemptd category', 'btemptd' ),
-                'icon'  => 'wordpress',
-            ),
-        )
-    );
-}
-add_filter( 'block_categories', 'btemptd_block_categories', 10, 2 );
