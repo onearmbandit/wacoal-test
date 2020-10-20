f5072e05c1c576321eb4bddcb8a93424146769b6
Prajna <prajna.mishra@wwindia.com>
2020-10-20 15:48:42 +0530

    feat b you page with explore blog integration

--- themes/btemptd/template-parts/front-page.php
+++ themes/btemptd/template-parts/front-page.php
@@ -190,42 +190,6 @@
 <!-- Entry page full width Slider -->
 
 <?php if(!empty($recent_posts)):?>
-<section class="explore-blog">
-    <div class="explore-blog--title">Explore the blog</div>
-
-    <div class="explore-blog--bg">
-    <div class="explore-blog--wrapper">
-        <?php foreach($recent_posts as $key =>$recent_post):
-            $thumbnail_id = get_post_thumbnail_id($recent_post->ID);
-            $thumbnail_url = Btemptd_Get_image(wp_get_attachment_image_src($thumbnail_id, 'full'));
-            $thumbnail_alt = Btemptd_Get_Image_alt($thumbnail_id, 'featured-img');
-            $categories = Btemptd_Get_Primary_category($recent_post->ID);
-            ?>
-            <div class="explore-blog--box">
-                <div class="explore-blog--image">
-                    <img class="img-fluid" src="<?php echo esc_url($thumbnail_url); ?>" alt="<?php echo esc_url($thumbnail_alt); ?>"/>
-                </div>
-
-                <div class="explore-blog--content">
-                    <div class="explore-blog--content__cta">
-                        <a href="<?php echo get_permalink($recent_post->ID);?>">
-                            <img src="<?php echo  esc_url(THEMEURI); ?>/assets/images/cta-down-arrow.svg" />
-                        </a>
-                    </div>
-                    <div class="explore-blog--content__category">
-                        <?php echo esc_attr($categories->name);?>
-                    </div>
-                    <div class="explore-blog--content__title">
-                        <?php echo esc_attr(get_the_title($recent_post->ID));?>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach;?>
-
-    </div>
-    </div>
-
-
-</section>
+    <?php require locate_template('template-parts/explore-page.php');?>
 <?php endif;?>
 
