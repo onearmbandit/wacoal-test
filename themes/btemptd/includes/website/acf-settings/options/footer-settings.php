35a60969d930197239e13c983d3069a1ced0cec9
vidyapawar95 <vpawar@cemtrexlabs.com>
2020-10-21 14:04:53 +0530

    Refactor
    - Added custom toolabr to footer copyright section.

--- themes/btemptd/includes/website/acf-settings/options/footer-settings.php
+++ themes/btemptd/includes/website/acf-settings/options/footer-settings.php
@@ -10,7 +10,6 @@
  * @link     Btemptd
  */
 
-
 acf_add_local_field_group(
     array(
     'key' => 'group_5f843efa4d26c',
@@ -27,11 +26,11 @@ acf_add_local_field_group(
             'wrapper' => array(
                 'width' => '',
                 'class' => '',
-                'id' => '',
+                'id' => 'footer_content',
             ),
             'default_value' => '',
             'tabs' => 'all',
-            'toolbar' => 'full',
+            'toolbar' => 'content_toolbar',
             'media_upload' => 1,
             'delay' => 0,
         ),
@@ -332,6 +331,4 @@ acf_add_local_field_group(
     'description' => '',
     )
 );
-
-
 
