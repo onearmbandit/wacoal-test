fdbb92a545927cd2de99afb8fbe6b82cbac93573
vidyapawar95 <vpawar@cemtrexlabs.com>
2020-11-02 13:45:09 +0530

    Refactor
    - Added links to images and title in pagination.

--- themes/wacoal/template-parts/content-excerpt.php
+++ themes/wacoal/template-parts/content-excerpt.php
@@ -12,16 +12,20 @@
 
 ?>
 <article class="blog-tile">
-    <div class="blog-tile--image">
-        <?php
-        $thumbnail_id  = get_post_thumbnail_id();
-        $thumbnail_url = Wacoal_Get_image(wp_get_attachment_image_src($thumbnail_id, 'full'));
-        $thumbnail_alt = Wacoal_Get_Image_alt($thumbnail_id, 'featured-img');
-        $post_tagline  = get_field('tag_line');
-        ?>
-        <img class="lazyload" data-src="<?php echo esc_url($thumbnail_url);?>"
-        src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="<?php echo esc_attr($thumbnail_alt);?>" />
-    </div>
+
+    <?php
+    $thumbnail_id  = get_post_thumbnail_id();
+    $thumbnail_url = Wacoal_Get_image(wp_get_attachment_image_src($thumbnail_id, 'full'));
+    $thumbnail_alt = Wacoal_Get_Image_alt($thumbnail_id, 'featured-img');
+    $post_tagline  = get_field('tag_line');
+    ?>
+
+    <a href="<?php echo esc_url(get_permalink());?>">
+        <div class="blog-tile--image">
+            <img class="lazyload" data-src="<?php echo esc_url($thumbnail_url);?>"
+            src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="<?php echo esc_attr($thumbnail_alt);?>" />
+        </div>
+    </a>
     <div class="blog-tile--category">
     <?php echo esc_attr($cat_name); ?>
     </div>
