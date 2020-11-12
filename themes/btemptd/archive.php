e0431241728016df0bcf522d34442cc92dcf9620
vidyapawar95 <vpawar@cemtrexlabs.com>
2020-11-12 19:07:44 +0530

    Fix

--- themes/btemptd/archive.php
+++ themes/btemptd/archive.php
@@ -19,16 +19,11 @@ $cat_name         = $current_cat_data->name;
 ?>
 
 <section class="banner-with-background">
-
-<?php if($current_cat_id && !empty($current_cat_id)) : ?>
     <h2 class="banner-with-background--heading"><?php echo esc_attr($cat_name);?></h2>
-<?php endif;?>
 
-<?php if(category_description() && !empty(category_description())) :?>
     <div class="banner-with-background--subtitle">
         <?php echo category_description(); ?>
     </div>
-<?php endif;?>
 
 </section>
 
