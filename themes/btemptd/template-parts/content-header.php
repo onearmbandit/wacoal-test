1046761ba718f4765ccc22c8b50a2f949ee07a24
Tushar F <tfirake@cemtrexlabs.com>
2021-03-18 16:07:05 +0530

    Feat - Btemptd search page responsive design updated

--- themes/btemptd/template-parts/content-header.php
+++ themes/btemptd/template-parts/content-header.php
@@ -19,10 +19,25 @@ $logo_image_url = Btemptd_Get_Image($logo_image_array);
     <a href="<?php echo esc_url(home_url());?>">
         <img class="header-section--logo" src="<?php echo esc_url($logo_image_url);?> " alt="<?php echo wp_kses_post($logo_image_alt);?>">
     </a>
-    <a class="shop-btemptd-btn shop-btemptd-btn-desktop" href="<?php echo esc_url(get_field('header_button_link', 'options'));?>" target="_blank">
+    <!-- <a class="shop-btemptd-btn shop-btemptd-btn-desktop" href="<?php echo esc_url(get_field('header_button_link', 'options'));?>" target="_blank">
     <?php echo esc_html(get_field('header_button_text', 'options'));?>   </a>
     <a class="shop-btemptd-btn shop-btemptd-btn-mobile" href="<?php echo esc_url(get_field('header_button_link', 'options'));?>" target="_blank">
-    <?php echo esc_html(get_field('mobile_header_button_text', 'options'));?></a>
+    <?php echo esc_html(get_field('mobile_header_button_text', 'options'));?></a> -->
+
+    <div class="header-search">
+        <!-- <div class="search-icon"></div> -->
+        <form class="es-form js-search-form" action="<?php echo esc_url(home_url('/')); ?>" method="get">
+            <div class="header-search--inner">
+                <input id="search-results-form-q"
+                    name="s"
+                    class="search-input js-search-input"
+                    value="<?php echo get_search_query(); ?>"
+                    placeholder="Search"
+                    type="search">
+                <button class="search-btn"></button>
+            </div>
+        </form>
+    </div>
 </header>
 <nav class="header-navigation">
     <div class="header-navigation-mobile">
@@ -43,13 +58,5 @@ $logo_image_url = Btemptd_Get_Image($logo_image_array);
     );
                                                 wp_nav_menu($args); ?>
 </nav>
-                    <div class="search-icon"></div>
-                    <form class="es-form js-search-form" action="<?php echo esc_url(home_url('/')); ?>" method="get">
-                        <input id="search-results-form-q"
-                            name="s"
-                            class="search-input js-search-input"
-                            value="<?php echo get_search_query(); ?>"
-                            placeholder="Search"
-                            type="search">
-                    </form>
+
                 </div>
