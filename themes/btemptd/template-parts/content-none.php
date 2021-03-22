efdd9d501468f2d9f6eee30451b7173e5dd77cf6
Tushar F <tfirake@cemtrexlabs.com>
2021-03-22 15:34:31 +0530

    Feat - No search found page updated

--- themes/btemptd/template-parts/content-none.php
+++ themes/btemptd/template-parts/content-none.php
@@ -29,11 +29,11 @@ $counts= $total_posts->publish;
     <p><?php echo esc_attr($res_found);?> results for “<?php echo wp_kses_post($search_word);?>”</p>
     </div>
 </div>
-<div class="para">
-<h4>No posts were found.</h4>
-    Please try a different search term or
-    <a href="<?php echo esc_url(home_url());?>"> go back to the homepage.</a>
-    </a>
+<div class="no-search">
+    <div class="search-container">
+        <h4>No posts were found.</h4>
+        <p>Please try a different search term or <a href="<?php echo esc_url(home_url());?>"> go back to the homepage.</a></p>
+    </div>
 </div>
 
 <?php if(!empty($recent_posts)) :?>
