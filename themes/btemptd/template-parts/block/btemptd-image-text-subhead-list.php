6b6e211425e4d05abb506f1db6bd81a629826e4a
swapniltoab <stawar@cemtrexlabs.com>
2021-06-11 12:57:01 +0530

    Fixed block design issue

--- themes/btemptd/template-parts/block/btemptd-image-text-subhead-list.php
+++ themes/btemptd/template-parts/block/btemptd-image-text-subhead-list.php
@@ -12,7 +12,7 @@
 
 ?>
 
-<section class="list-text-image">
+<section class="italics-title">
 
     <?php foreach ($list_data as $key => $list) {
             $title = $list['title'];
@@ -21,41 +21,45 @@
 
             $image_id = $list['image'];
             $image_array = wp_get_attachment_image_src($image_id, 'full');
-            $image_alt   = Btemptd_Get_Image_alt($image_id, 'Block Image');
             $image_url   = Btemptd_Get_image($image_array);
 
             $image_caption = $list['image_caption'];
             $image_link = $list['image_link'];
             $new_tab = $list['open_link_in_new_tab'] === true ? '_blank' : '_self';
         ?>
-        <div class="list-text-image--wrapper">
-            <div class="list-text-image--inner">
 
-                <?php if (! empty($image_link)) { ?>
-                    <a href="<?php echo esc_url($image_link) ?>"
-                        target="<?php echo esc_attr($new_tab) ?>" >
-                        <div class="list-text-image--img" style="background-image:url(<?php echo esc_url($image_url); ?>)"></div>
-                    </a>
-                <?php } else { ?>
-                        <div class="list-text-image--img" style="background-image:url(<?php echo esc_url($image_url); ?>)"></div>
-                <?php } ?>
+    <div class="italics-title--wrapper">
+        <div class="left-box">
+            <div>
+                <h3 class="title">
+                    <?php echo esc_attr($title); ?>
+                </h3>
+                <div class="sub-title">
+                    <?php echo esc_attr($subtitlte); ?>
+                </div>
 
-                <div class="image-name mobile">
-                    <?php echo wp_kses_post($image_caption); ?>
+                <div class="content">
+                    <?php echo wp_kses_post($description); ?>
                 </div>
-                <div class="list-text-image--content">
-                    <h2 class="title"><?php echo esc_attr($title); ?></h2>
-                    <h4 class="subtitle"><?php echo esc_attr($subtitlte); ?></h4>
-                    <div class="content">
-                        <?php echo wp_kses_post($description); ?>
-                    </div>
-                </div>
-            </div>
-            <div class="image-name desktop">
-                <?php echo wp_kses_post($image_caption); ?>
             </div>
         </div>
 
-    <?php } ?>
+        <div class="right-box">
+            <?php if (! empty($image_link)) { ?>
+            <a href="<?php echo esc_url($image_link) ?>"
+                    target="<?php echo esc_attr($new_tab) ?>" >
+                <div class="image-wrapper" style="background-image:url(<?php echo esc_url($image_url); ?>);">
+                </div>
+            </a>
+            <?php } else { ?>
+                <div class="image-wrapper" style="background-image:url(<?php echo esc_url($image_url); ?>);">
+                </div>
+            <?php } ?>
+            <div class="image-title">
+                <?php echo wp_kses_post($image_caption); ?>
+            </div>
+        </div>
+    </div>
 
+    <?php } ?>
 </section>
