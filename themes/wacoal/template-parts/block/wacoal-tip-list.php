bb3421311c5f0ca556c738867edb2f9a35a33698
swapniltoab <stawar@cemtrexlabs.com>
2021-06-23 17:28:33 +0530

    Changes added

--- themes/wacoal/template-parts/block/wacoal-tip-list.php
+++ themes/wacoal/template-parts/block/wacoal-tip-list.php
@@ -14,6 +14,7 @@
 <?php if ($block_lists && !empty($block_lists)) { ?>
 <section class="text-image-tip desktop">
     <?php foreach ($block_lists as $key => $list) {
+            $image_position     = $list['image_position'];
             $title              = $list['title'];
             $subtitle           = $list['subtitle'];
             $bullet_points      = $list['bullet_points'];
@@ -29,107 +30,107 @@
             $list_image_url   = Wacoal_Get_image($list_image_array);
         }
 
-        if ($key % 2 == 0) {
+        if ($image_position == 'left') {
             ?>
-    <div class="text-image-tip--wrapper">
+            <div class="text-image-tip--wrapper img-left">
 
-            <?php if($list_img_id && !empty($list_img_id)) :
-                if(!empty($img_link)) :?>
-            <a href="<?php echo esc_url($img_link);?>" <?php if($new_tab == true) : echo "target='_blank'";
-           endif;?>>
-                <?php endif;?>
-        <div class="image" style="background-image:url(<?php echo  esc_url($list_image_url); ?>">
-        </div>
-                <?php if(!empty($img_link)) :?>
-        </a>
-                <?php endif;
-            endif;?>
-
-        <div class="content">
-
-            <?php if($title && !empty($title)) :?>
-        <h1 class="head">
-                <?php echo wp_kses_post($title);?>
-        </h1>
-            <?php endif;?>
-
-            <?php if($subtitle && !empty($subtitle)) :?>
-            <div class="sub-head">
-                <?php echo wp_kses_post($subtitle);?>
-            </div>
-            <?php endif;?>
-
-            <?php if($bullet_points && !empty($bullet_points)) :
-                echo Wacoal_Remove_P_tag(wp_kses_post($bullet_points));
-            endif;?>
-
-            <div class="tip">
-            <?php if($tip_title && !empty($tip_title)) :?>
-                <div class="title">
-                    <?php echo wp_kses_post($tip_title);?>
+                    <?php if($list_img_id && !empty($list_img_id)) :
+                        if(!empty($img_link)) :?>
+                    <a href="<?php echo esc_url($img_link);?>" <?php if($new_tab == true) : echo "target='_blank'";
+                   endif;?>>
+                        <?php endif;?>
+                <div class="image" style="background-image:url(<?php echo  esc_url($list_image_url); ?>">
                 </div>
-            <?php endif;?>
+                        <?php if(!empty($img_link)) :?>
+                </a>
+                        <?php endif;
+                    endif;?>
 
-            <?php if($tip_content && !empty($tip_content)) :?>
-                <div class="tip-content">
-                    <?php echo wp_kses_post($tip_content);?>
-                </div>
-            <?php endif;?>
+                <div class="content">
 
-            </div>
-        </div>
-    </div>
-            <?php
-        } elseif ($key % 2 == 1) {
-            ?>
-    <div class="text-image-tip--wrapper">
-
-            <?php if($list_img_id && !empty($list_img_id)) :
-                if(!empty($img_link)) :?>
-            <a href="<?php echo esc_url($img_link);?>" <?php if($new_tab == true) : echo "target='_blank'";
-           endif;?>>
-                <?php endif;?>
-        <div class="image" style="background-image:url(<?php echo esc_url($list_image_url); ?>">
-        </div>
-                <?php if(!empty($img_link)) :?>
-        </a>
-                <?php endif;
-            endif;?>
-
-        <div class="content">
-            <?php if($title && !empty($title)) :?>
+                    <?php if($title && !empty($title)) :?>
                 <h1 class="head">
-                    <?php echo wp_kses_post($title);?>
+                        <?php echo wp_kses_post($title);?>
                 </h1>
-            <?php endif;?>
+                    <?php endif;?>
 
-            <?php if($subtitle && !empty($subtitle)) :?>
-            <div class="sub-head">
-                <?php echo wp_kses_post($subtitle);?>
-            </div>
-            <?php endif;?>
+                    <?php if($subtitle && !empty($subtitle)) :?>
+                    <div class="sub-head">
+                        <?php echo wp_kses_post($subtitle);?>
+                    </div>
+                    <?php endif;?>
 
-            <?php if($bullet_points && !empty($bullet_points)) :
-                echo Wacoal_Remove_P_tag(wp_kses_post($bullet_points));
-            endif;?>
+                    <?php if($bullet_points && !empty($bullet_points)) :
+                        echo Wacoal_Remove_P_tag(wp_kses_post($bullet_points));
+                    endif;?>
 
-            <div class="tip">
+                    <div class="tip">
+                    <?php if($tip_title && !empty($tip_title)) :?>
+                        <div class="title">
+                            <?php echo wp_kses_post($tip_title);?>
+                        </div>
+                    <?php endif;?>
 
-            <?php if($tip_title && !empty($tip_title)) :?>
-                <div class="title">
-                    <?php echo wp_kses_post($tip_title);?>
+                    <?php if($tip_content && !empty($tip_content)) :?>
+                        <div class="tip-content">
+                            <?php echo wp_kses_post($tip_content);?>
+                        </div>
+                    <?php endif;?>
+
+                    </div>
                 </div>
-            <?php endif;?>
-
-            <?php if($tip_content && !empty($tip_content)) :?>
-                <div class="tip-content">
-                    <?php echo wp_kses_post($tip_content);?>
-                </div>
-            <?php endif;?>
-
             </div>
-        </div>
-    </div>
+            <?php
+        } elseif ($image_position == 'right') {
+            ?>
+            <div class="text-image-tip--wrapper img-right">
+
+                    <?php if($list_img_id && !empty($list_img_id)) :
+                        if(!empty($img_link)) :?>
+                    <a href="<?php echo esc_url($img_link);?>" <?php if($new_tab == true) : echo "target='_blank'";
+                   endif;?>>
+                        <?php endif;?>
+                <div class="image" style="background-image:url(<?php echo esc_url($list_image_url); ?>">
+                </div>
+                        <?php if(!empty($img_link)) :?>
+                </a>
+                        <?php endif;
+                    endif;?>
+
+                <div class="content">
+                    <?php if($title && !empty($title)) :?>
+                        <h1 class="head">
+                            <?php echo wp_kses_post($title);?>
+                        </h1>
+                    <?php endif;?>
+
+                    <?php if($subtitle && !empty($subtitle)) :?>
+                    <div class="sub-head">
+                        <?php echo wp_kses_post($subtitle);?>
+                    </div>
+                    <?php endif;?>
+
+                    <?php if($bullet_points && !empty($bullet_points)) :
+                        echo Wacoal_Remove_P_tag(wp_kses_post($bullet_points));
+                    endif;?>
+
+                    <div class="tip">
+
+                    <?php if($tip_title && !empty($tip_title)) :?>
+                        <div class="title">
+                            <?php echo wp_kses_post($tip_title);?>
+                        </div>
+                    <?php endif;?>
+
+                    <?php if($tip_content && !empty($tip_content)) :?>
+                        <div class="tip-content">
+                            <?php echo wp_kses_post($tip_content);?>
+                        </div>
+                    <?php endif;?>
+
+                    </div>
+                </div>
+            </div>
         <?php }
     }?>
 </section>
@@ -138,14 +139,15 @@
 <?php if ($block_lists && !empty($block_lists)) { ?>
 <section class="text-image-tip mobile">
     <?php foreach ($block_lists as $key => $list) {
-                $title              = $list['title'];
-                $subtitle           = $list['subtitle'];
-                $bullet_points      = $list['bullet_points'];
-                $tip_title          = $list['tip_title'];
-                $tip_content        = $list['tip_content'];
-                $list_img_id        = $list['image'];
-                $img_link           = $list['image_link'];
-                $new_tab            = $list['open_in_new_tab'];
+            $image_position     = $list['image_position'];
+            $title              = $list['title'];
+            $subtitle           = $list['subtitle'];
+            $bullet_points      = $list['bullet_points'];
+            $tip_title          = $list['tip_title'];
+            $tip_content        = $list['tip_content'];
+            $list_img_id        = $list['image'];
+            $img_link           = $list['image_link'];
+            $new_tab            = $list['open_in_new_tab'];
 
         if (! empty($list_img_id) && $list_img_id ) {
             $list_image_array = wp_get_attachment_image_src($list_img_id, 'full');
@@ -153,108 +155,108 @@
             $list_image_url   = Wacoal_Get_image($list_image_array);
         }
 
-        if ($key % 2 == 0) { ?>
-    <div class="text-image-tip--wrapper">
+        if ($image_position == 'left') { ?>
+            <div class="text-image-tip--wrapper img-left">
 
-        <div class="content">
+                <div class="content">
 
-            <?php if($title && !empty($title)) :?>
-                <h1 class="head">
-                    <?php echo wp_kses_post($title);?>
-                </h1>
-            <?php endif;?>
+                    <?php if($title && !empty($title)) :?>
+                        <h1 class="head">
+                            <?php echo wp_kses_post($title);?>
+                        </h1>
+                    <?php endif;?>
 
-            <?php if($subtitle && !empty($subtitle)) :?>
-                <div class="sub-head">
-                    <?php echo wp_kses_post($subtitle);?>
+                    <?php if($subtitle && !empty($subtitle)) :?>
+                        <div class="sub-head">
+                            <?php echo wp_kses_post($subtitle);?>
+                        </div>
+                    <?php endif;?>
+
                 </div>
-            <?php endif;?>
 
-        </div>
+                    <?php if($list_img_id && !empty($list_img_id)) :
+                        if(!empty($img_link)) :?>
+                            <a href="<?php echo esc_url($img_link);?>">
+                        <?php endif;?>
+                        <div class="image" style="background-image:url(<?php echo  esc_url($list_image_url); ?>">
+                        </div>
+                        <?php if(!empty($img_link)) :?>
+                            </a>
+                        <?php endif;
+                    endif;?>
 
-            <?php if($list_img_id && !empty($list_img_id)) :
-                if(!empty($img_link)) :?>
-                    <a href="<?php echo esc_url($img_link);?>">
-                <?php endif;?>
-                <div class="image" style="background-image:url(<?php echo  esc_url($list_image_url); ?>">
+                <div class="content">
+                    <?php if($bullet_points && !empty($bullet_points)) :
+                        echo Wacoal_Remove_P_tag(wp_kses_post($bullet_points));
+                    endif;?>
+
+                    <div class="tip">
+
+                    <?php if($tip_title && !empty($tip_title)) :?>
+                        <div class="title">
+                            <?php echo wp_kses_post($tip_title);?>
+                        </div>
+                    <?php endif;?>
+
+                    <?php if($tip_content && !empty($tip_content)) :?>
+                        <div class="tip-content">
+                            <?php echo wp_kses_post($tip_content);?>
+                        </div>
+                    <?php endif;?>
+
+                    </div>
                 </div>
-                <?php if(!empty($img_link)) :?>
-                    </a>
-                <?php endif;
-            endif;?>
-
-        <div class="content">
-            <?php if($bullet_points && !empty($bullet_points)) :
-                echo Wacoal_Remove_P_tag(wp_kses_post($bullet_points));
-            endif;?>
-
-            <div class="tip">
-
-            <?php if($tip_title && !empty($tip_title)) :?>
-                <div class="title">
-                    <?php echo wp_kses_post($tip_title);?>
-                </div>
-            <?php endif;?>
-
-            <?php if($tip_content && !empty($tip_content)) :?>
-                <div class="tip-content">
-                    <?php echo wp_kses_post($tip_content);?>
-                </div>
-            <?php endif;?>
-
             </div>
-        </div>
-    </div>
             <?php
-        } elseif ($key % 2 == 1) { ?>
-    <div class="text-image-tip--wrapper">
-        <div class="content">
+        } elseif ($image_position == 'right') { ?>
+            <div class="text-image-tip--wrapper img-right">
+                <div class="content">
 
-            <?php if($title && !empty($title)) :?>
-                <h1 class="head">
-                    <?php echo wp_kses_post($title);?>
-                </h1>
-            <?php endif;?>
+                    <?php if($title && !empty($title)) :?>
+                        <h1 class="head">
+                            <?php echo wp_kses_post($title);?>
+                        </h1>
+                    <?php endif;?>
 
-            <?php if($subtitle && !empty($subtitle)) :?>
-                <div class="sub-head">
-                    <?php echo wp_kses_post($subtitle);?>
+                    <?php if($subtitle && !empty($subtitle)) :?>
+                        <div class="sub-head">
+                            <?php echo wp_kses_post($subtitle);?>
+                        </div>
+                    <?php endif;?>
+
                 </div>
-            <?php endif;?>
 
-        </div>
+                    <?php if($list_img_id && !empty($list_img_id)) :
+                        if(!empty($img_link)) :?>
+                            <a href="<?php echo esc_url($img_link);?>">
+                        <?php endif;?>
+                        <div class="image" style="background-image:url(<?php echo  esc_url($list_image_url); ?>">
+                        </div>
+                        <?php if(!empty($img_link)) :?>
+                            </a>
+                        <?php endif;
+                    endif;?>
 
-            <?php if($list_img_id && !empty($list_img_id)) :
-                if(!empty($img_link)) :?>
-                    <a href="<?php echo esc_url($img_link);?>">
-                <?php endif;?>
-                <div class="image" style="background-image:url(<?php echo  esc_url($list_image_url); ?>">
+                <div class="content">
+                    <?php if($bullet_points && !empty($bullet_points)) :
+                        echo Wacoal_Remove_P_tag(wp_kses_post($bullet_points));
+                    endif;?>
+
+                    <div class="tip">
+                    <?php if($tip_title && !empty($tip_title)) :?>
+                        <div class="title">
+                            <?php echo wp_kses_post($tip_title);?>
+                        </div>
+                    <?php endif;?>
+
+                    <?php if($tip_content && !empty($tip_content)) :?>
+                        <div class="tip-content">
+                            <?php echo wp_kses_post($tip_content);?>
+                        </div>
+                    <?php endif;?>
+                    </div>
                 </div>
-                <?php if(!empty($img_link)) :?>
-                    </a>
-                <?php endif;
-            endif;?>
-
-        <div class="content">
-            <?php if($bullet_points && !empty($bullet_points)) :
-                echo Wacoal_Remove_P_tag(wp_kses_post($bullet_points));
-            endif;?>
-
-            <div class="tip">
-            <?php if($tip_title && !empty($tip_title)) :?>
-                <div class="title">
-                    <?php echo wp_kses_post($tip_title);?>
-                </div>
-            <?php endif;?>
-
-            <?php if($tip_content && !empty($tip_content)) :?>
-                <div class="tip-content">
-                    <?php echo wp_kses_post($tip_content);?>
-                </div>
-            <?php endif;?>
             </div>
-        </div>
-    </div>
         <?php }
     }?>
 </section>
