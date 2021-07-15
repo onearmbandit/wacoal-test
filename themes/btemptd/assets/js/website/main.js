4161e4a1356a124530442f03afad78eb1d4e4ea5
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-07-15 19:01:16 +0530

    Fix
    - Fixed button issue.

--- themes/btemptd/assets/js/website/main.js
+++ themes/btemptd/assets/js/website/main.js
@@ -3,26 +3,25 @@
   $(document).on('click', '.see-more-button', function (event) {
 
     var input = $("#offset").val();
-    var offset= parseInt(input)+3;
+    var offset = parseInt(input) + 3;
     var total = $("#total").val();
     $.ajax({
       url: btemptd_js_var.ajaxurl,
       type: 'post',
       data: {
         action: 'btemptd_load_more',
-        cat_id:$("#cat_id").val(),
-        offset:offset,
-        nonce:btemptd_js_var.nonce,
-        post_id:$("#hidden_post").val(),
+        cat_id: $("#cat_id").val(),
+        offset: offset,
+        nonce: btemptd_js_var.nonce,
+        post_id: $("#hidden_post").val(),
       },
       success: function (html) {
         $("#offset").val(offset);
-        $(html).insertAfter( $( ".explore-see-more" ).last() );
-        $(window).scrollTop($(".explore-see-more").last().offset().top-180);
+        $(html).insertAfter($(".explore-see-more").last());
+        $(window).scrollTop($(".explore-see-more").last().offset().top - 180);
 
-        var article_count= $(".explore-see-more .explore-blog--box").length;
-        console.log(article_count);
-        if(html == 0 || total == article_count){
+        var article_count = $(".explore-see-more .explore-blog--box").length;
+        if (html == 0 || total == article_count) {
 
           $(".see-more-button").addClass("disabled");
           $(".see-more-button").hide();
@@ -35,31 +34,27 @@
   $(document).on('click', '.cat-see-more-button', function (event) {
 
     var input = $("#cat_offset").val();
-    var offset= parseInt(input)+6;
+    var offset = parseInt(input) + 6;
     var total = $("#cat_total").val();
     $.ajax({
       url: btemptd_js_var.ajaxurl,
       type: 'post',
       data: {
         action: 'btemptd_cat_posts_load_more',
-        cat_id:$("#cate_id").val(),
-        offset:offset,
-        nonce:btemptd_js_var.nonce,
+        cat_id: $("#cate_id").val(),
+        offset: offset,
+        nonce: btemptd_js_var.nonce,
       },
       success: function (html) {
         $("#cat_offset").val(offset);
-        $(html).insertAfter( $( ".cat-post-listing" ).last() );
-        $(window).scrollTop($(".cat-post-listing").last().offset().top-180);
+        $(html).insertAfter($(".cat-post-listing").last());
+        $(window).scrollTop($(".cat-post-listing").last().offset().top - 180);
 
-        setTimeout(() => {
-          // var article_count_load_more= ($(".cat-post-listing .explore-blog--box").length - 6 ) / 2;
-          // var article_count = article_count_load_more + 6;
-          var article_count= $(".cat-post-listing .explore-blog--box").length;
-          if(html == 0 || article_count == total){
-            $(".cat-see-more-button").addClass("disabled");
-            $(".cat-see-more-button").hide();
-          }
-        } , 500);
+        var article_count = $(".cat-post-listing .explore-blog--box").length;
+        if (html == 0 || total == article_count) {
+          $(".cat-see-more-button").addClass("disabled");
+          $(".cat-see-more-button").hide();
+        }
 
       }
     });
