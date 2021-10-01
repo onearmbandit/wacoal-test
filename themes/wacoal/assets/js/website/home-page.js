93ced1dee70d6ac8ba259269a2501d0e88c015f6
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-10-01 16:59:28 +0530

    Fix.

--- themes/wacoal/assets/js/website/home-page.js
+++ themes/wacoal/assets/js/website/home-page.js
@@ -18,8 +18,36 @@
         $(html).insertAfter($(".more-blog").last());
         $(window).scrollTop($(".more-blog").last().offset().top-180);
         var article_count= $("article.blog-tile").length;
-        if(html == 0 || total == article_count){
+        console.log('total-->',total);
+        console.log('article_count-->',article_count);
+        if(html == 0 || total == article_count ){
           $('.more').parent('div').addClass('disable');
+        }
+
+      }
+    });
+  });
+
+  $(document).on('click', '.search-more', function (event) {
+    var input= $("#offset").val();
+    var offset= parseInt(input)+3;
+    var total= $("#total").val();
+
+    $.ajax({
+      url: wacoal_js_var.ajaxurl,
+      type: 'post',
+      data: {
+        action: 'wacoal_search_load_more',
+        offset:offset,
+        nonce:wacoal_js_var.nonce,
+      },
+      success: function (html) {
+        $("#offset").val(offset);
+        $(html).insertAfter($(".more-blog").last());
+        $(window).scrollTop($(".more-blog").last().offset().top-180);
+        var article_count= $("article.blog-tile").length;
+        if(html == 0 || total == article_count){
+          $('.search-more').parent('div').addClass('disable');
         }
 
       }
