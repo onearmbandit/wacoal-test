812835d8f2c477b00de1719c0b8452ee992f0a5c
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-10-06 16:27:39 +0530

    Feat
    - Removed slider settings from catefory.

--- themes/btemptd/includes/website/acf-settings/options/category-settings.php
+++ themes/btemptd/includes/website/acf-settings/options/category-settings.php
@@ -193,35 +193,6 @@ acf_add_local_field_group(
             'max' => '',
             'return_format' => 'id',
         ),
-        array(
-            'key' => 'field_5f8ee153add97',
-            'label' => 'Slider posts',
-            'name' => 'slider_posts',
-            'type' => 'relationship',
-            'instructions' => '',
-            'required' => 0,
-            'conditional_logic' => 0,
-            'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
-            ),
-            'post_type' => array(
-                0 => 'post',
-            ),
-            'taxonomy' => '',
-            'filters' => array(
-                0 => 'search',
-                1 => 'post_type',
-                2 => 'taxonomy',
-            ),
-            'elements' => array(
-                0 => 'featured_image',
-            ),
-            'min' => '',
-            'max' => '',
-            'return_format' => 'id',
-        ),
     ),
     'location' => array(
         array(
