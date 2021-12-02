e04f3e73c195d4d3b8b4f500f3463f9feb49572c
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-12-02 18:52:27 +0530

    Feat
    - Updated wordpress SEO plugin to latest version.

--- plugins/wordpress-seo/vendor/autoload.php
+++ plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite9c6af6df20f390378e3da43bcac09a2::getLoader();
+return ComposerAutoloaderInit1784eb03537fd1492f50df46ca25d00c::getLoader();
