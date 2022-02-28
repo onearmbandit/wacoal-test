57916459fba0e52def4eb76b9493d823f2e08142
bhumi-cemtrex <bhpatel@cemtrexlabs.com>
2022-02-28 13:36:06 +0530

    Btemptd pattern issue fixed

--- themes/btemptd/includes/block-patterns/patterns/image-quote-button-right.php
+++ themes/btemptd/includes/block-patterns/patterns/image-quote-button-right.php
@@ -7,38 +7,38 @@ return array(
 	'categories' => array( 'text' ),
 	'blockTypes' => array( 'core/template-part/text' ),
 	'content'    => '<!-- wp:group {"tagName":"section","className":"image-quote-button"} -->
- <section class="wp-block-group image-quote-button"><!-- wp:columns {"verticalAlignment":"center","className":"image-quote-button\u002d\u002dwrapper"} -->
- <div class="wp-block-columns are-vertically-aligned-center image-quote-button--wrapper"><!-- wp:column {"verticalAlignment":"center","className":"content-wrapper content-wrapper\u002d\u002dinner"} -->
- <div class="wp-block-column is-vertically-aligned-center content-wrapper content-wrapper--inner"><!-- wp:image {"sizeSlug":"large","className":"quote-left"} -->
- <figure class="wp-block-image size-large quote-left"><img src="https://btemptdblog.wacoal-america.mark4.cetxlabs.com/wp-content/themes/btemptd/assets/images/quote-left.svg" alt=""/></figure>
- <!-- /wp:image -->
+	<section class="wp-block-group image-quote-button"><!-- wp:columns {"verticalAlignment":"center","className":"image-quote-button\u002d\u002dwrapper  img-right"} -->
+	<div class="wp-block-columns are-vertically-aligned-center image-quote-button--wrapper  img-right"><!-- wp:column {"verticalAlignment":"center","width":"","className":"image-wrapper"} -->
+	<div class="wp-block-column is-vertically-aligned-center image-wrapper"><!-- wp:image {"sizeSlug":"large","linkDestination":"none","className":"image-name"} -->
+	<figure class="wp-block-image size-large image-name"><img src="https://btemptdblog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/sites/3/2020/10/BT_BEATRIZA_596X596.png" alt=""/></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:column -->
 
- <!-- wp:group {"className":"content-inner"} -->
- <div class="wp-block-group content-inner"><!-- wp:paragraph {"className":"content-inner\u002d\u002dtitle"} -->
- <p class="content-inner--title" id="image-text-left-right-left-image">Image + Quote + Caption + Button (Left &amp; Right)<br>It’s so soft and comfortable I almost forget I’m wearing it!</p>
- <!-- /wp:paragraph -->
+	<!-- wp:column {"verticalAlignment":"center","className":"content-wrapper content-wrapper\u002d\u002dinner"} -->
+	<div class="wp-block-column is-vertically-aligned-center content-wrapper content-wrapper--inner"><!-- wp:image {"sizeSlug":"large","className":"quote-left"} -->
+	<figure class="wp-block-image size-large quote-left"><img src="https://btemptdblog.wacoal-america.mark4.cetxlabs.com/wp-content/themes/btemptd/assets/images/quote-left.svg" alt=""/></figure>
+	<!-- /wp:image -->
 
- <!-- wp:paragraph {"className":"content-inner\u002d\u002dtag"} -->
- <p class="content-inner--tag">LOREM IPSUM DOLOR S</p>
- <!-- /wp:paragraph -->
+	<!-- wp:group {"className":"content-inner"} -->
+	<div class="wp-block-group content-inner"><!-- wp:paragraph {"className":"content-inner\u002d\u002dtitle"} -->
+	<p class="content-inner--title" id="image-text-left-right-left-image">Image + Quote + Caption + Button (Left &amp; Right)<br>It’s so soft and comfortable I almost forget I’m wearing it!</p>
+	<!-- /wp:paragraph -->
 
- <!-- wp:buttons {"className":"shop-button","layout":{"type":"flex","justifyContent":"center"}} -->
- <div class="wp-block-buttons shop-button"><!-- wp:button {"style":{"border":{"radius":"0px"}},"className":"shop-now-button"} -->
- <div class="wp-block-button shop-now-button"><a class="wp-block-button__link" style="border-radius:0px">SHOP NOW</a></div>
- <!-- /wp:button --></div>
- <!-- /wp:buttons --></div>
- <!-- /wp:group -->
+	<!-- wp:paragraph {"className":"content-inner\u002d\u002dtag"} -->
+	<p class="content-inner--tag">LOREM IPSUM DOLOR S</p>
+	<!-- /wp:paragraph -->
 
- <!-- wp:image {"sizeSlug":"large","className":"quote-right"} -->
- <figure class="wp-block-image size-large quote-right"><img src="https://btemptdblog.wacoal-america.mark4.cetxlabs.com/wp-content/themes/btemptd/assets/images/quote-right.svg" alt=""/></figure>
- <!-- /wp:image --></div>
- <!-- /wp:column -->
+	<!-- wp:buttons {"className":"shop-button","layout":{"type":"flex","justifyContent":"center"}} -->
+	<div class="wp-block-buttons shop-button"><!-- wp:button {"style":{"border":{"radius":"0px"}},"className":"shop-now-button"} -->
+	<div class="wp-block-button shop-now-button"><a class="wp-block-button__link" style="border-radius:0px">SHOP NOW</a></div>
+	<!-- /wp:button --></div>
+	<!-- /wp:buttons --></div>
+	<!-- /wp:group -->
 
- <!-- wp:column {"verticalAlignment":"center","width":"","className":"image-wrapper"} -->
- <div class="wp-block-column is-vertically-aligned-center image-wrapper"><!-- wp:image {"sizeSlug":"large","linkDestination":"none","className":"image-name"} -->
- <figure class="wp-block-image size-large image-name"><img src="https://btemptdblog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/sites/3/2020/10/BT_BEATRIZA_596X596.png" alt=""/></figure>
- <!-- /wp:image --></div>
- <!-- /wp:column --></div>
- <!-- /wp:columns --></section>
- <!-- /wp:group -->',
+	<!-- wp:image {"sizeSlug":"large","className":"quote-right"} -->
+	<figure class="wp-block-image size-large quote-right"><img src="https://btemptdblog.wacoal-america.mark4.cetxlabs.com/wp-content/themes/btemptd/assets/images/quote-right.svg" alt=""/></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns --></section>
+	<!-- /wp:group -->',
 );
