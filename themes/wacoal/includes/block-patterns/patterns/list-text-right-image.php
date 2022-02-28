7b69a2fdccfeaa06fb10cdb99348e76bfa591c19
bhumi-cemtrex <bhpatel@cemtrexlabs.com>
2022-02-28 18:39:25 +0530

    wacoal pattern bug fixed

--- themes/wacoal/includes/block-patterns/patterns/list-text-right-image.php
+++ themes/wacoal/includes/block-patterns/patterns/list-text-right-image.php
@@ -6,29 +6,29 @@ return array(
 	'title'      => __( 'List Text Right Image', 'wacoal' ),
 	'categories' => array( 'text' ),
 	'blockTypes' => array( 'core/template-part/text' ),
-	'content'    => '<!-- wp:group {"tagName":"section","className":"list-text-image "} -->
- <section class="wp-block-group list-text-image"><!-- wp:columns {"verticalAlignment":"center","className":"list-text-image\u002d\u002dwrapper  img-right"} -->
- <div class="wp-block-columns are-vertically-aligned-center list-text-image--wrapper  img-right"><!-- wp:column {"verticalAlignment":"center","className":"ml-0"} -->
- <div class="wp-block-column is-vertically-aligned-center ml-0"><!-- wp:group {"className":"list-text-image\u002d\u002dcontent"} -->
- <div class="wp-block-group list-text-image--content"><!-- wp:heading {"className":"title"} -->
- <h2 class="title" id="for-smoothing">For Smoothing</h2>
- <!-- /wp:heading -->
+	'content'    => '<!-- wp:group {"tagName":"section","className":"list-text-image"} -->
+	<section class="wp-block-group list-text-image"><!-- wp:columns {"verticalAlignment":"center","className":"list-text-image\u002d\u002dwrapper img-right"} -->
+	<div class="wp-block-columns are-vertically-aligned-center list-text-image--wrapper img-right"><!-- wp:column {"verticalAlignment":"center"} -->
+	<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"className":"list-text-image\u002d\u002dcontent"} -->
+	<div class="wp-block-group list-text-image--content"><!-- wp:heading {"className":"title"} -->
+	<h2 class="title">For Smoothing</h2>
+	<!-- /wp:heading -->
 
- <!-- wp:paragraph {"className":"sub-title"} -->
- <p class="sub-title">Body Base® Shorty Panty</p>
- <!-- /wp:paragraph -->
+	<!-- wp:paragraph {"className":"sub-title"} -->
+	<p class="sub-title">Body Base® Shorty Panty</p>
+	<!-- /wp:paragraph -->
 
- <!-- wp:paragraph {"className":"content"} -->
- <p class="content">A hands-down cult favorite at Wacoal HQ, Body Base® is all about solutions—and we love it for that. It’s lightweight, smoothing, and helps clothing skim your body. Think of it as a supportive hybrid between panties and shapewear—only it’s not constricting. We truly cannot get enough of this style.</p>
- <!-- /wp:paragraph --></div>
- <!-- /wp:group --></div>
- <!-- /wp:column -->
+	<!-- wp:paragraph {"className":"content"} -->
+	<p class="content">A hands-down cult favorite at Wacoal HQ, Body Base® is all about solutions—and we love it for that. It’s lightweight, smoothing, and helps clothing skim your body. Think of it as a supportive hybrid between panties and shapewear—only it’s not constricting. We truly cannot get enough of this style.</p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:group --></div>
+	<!-- /wp:column -->
 
- <!-- wp:column {"verticalAlignment":"center","width":"auto"} -->
- <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:auto"><!-- wp:image {"id":250,"sizeSlug":"full","linkDestination":"none","className":"image-name"} -->
- <figure class="wp-block-image size-full image-name"><img src="https://blog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/2020/10/minimizer_desktop.jpeg" alt="" class="wp-image-250"/><figcaption>Body Base® Shorty Panty</figcaption></figure>
- <!-- /wp:image --></div>
- <!-- /wp:column --></div>
- <!-- /wp:columns --></section>
- <!-- /wp:group -->',
+	<!-- wp:column {"verticalAlignment":"center"} -->
+	<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"sizeSlug":"large","className":"image-name"} -->
+	<figure class="wp-block-image size-large image-name"><img src="https://blog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/2020/10/minimizer_desktop.jpeg" alt=""/><figcaption>Body Base® Shorty Panty</figcaption></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns --></section>
+	<!-- /wp:group -->',
 );
