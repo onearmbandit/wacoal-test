0a7fdc1a446f0ac1011bd267678c2754978c16c1
bhumi-cemtrex <bhpatel@cemtrexlabs.com>
2022-03-15 16:34:32 +0530

    add new wacoal patterns

---
+++ themes/wacoal/includes/block-patterns/patterns/product-gallery.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * product Gallery
+ */
+return array(
+	'title'      => __( 'product Gallery', 'wacoal' ),
+	'categories' => array( 'text' ),
+	'blockTypes' => array( 'core/template-part/text' ),
+	'content'    => '<!-- wp:group {"tagName":"section","className":"product-gallery"} -->
+ <section class="wp-block-group product-gallery"><!-- wp:columns {"className":"product-gallery\u002d\u002dwrapper"} -->
+ <div class="wp-block-columns product-gallery--wrapper"><!-- wp:column {"className":"product-gallery\u002d\u002dbox"} -->
+ <div class="wp-block-column product-gallery--box"><!-- wp:image {"sizeSlug":"large","linkDestination":"custom","className":"product-gallery\u002d\u002dbox__image"} -->
+ <figure class="wp-block-image size-large product-gallery--box__image"><a href="https://blog.wacoal-america.mark4.cetxlabs.com/sample-post-with-all-8-blocks/"><img src="https://blog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/2020/10/download-4-1.png" alt=""/></a></figure>
+ <!-- /wp:image -->
+
+ <!-- wp:paragraph {"className":"product-gallery\u002d\u002dbox__title"} -->
+ <p class="product-gallery--box__title"><a href="https://blog.wacoal-america.mark4.cetxlabs.com/sample-post-with-all-8-blocks/" target="_blank" rel="noreferrer noopener">Product Gallery</a></p>
+ <!-- /wp:paragraph -->
+
+ <!-- wp:paragraph {"className":"product-gallery\u002d\u002dbox__size"} -->
+ <p class="product-gallery--box__size">XL</p>
+ <!-- /wp:paragraph --></div>
+ <!-- /wp:column -->
+
+ <!-- wp:column {"className":"product-gallery\u002d\u002dbox"} -->
+ <div class="wp-block-column product-gallery--box"><!-- wp:image {"sizeSlug":"large","linkDestination":"custom","className":"product-gallery\u002d\u002dbox__image"} -->
+ <figure class="wp-block-image size-large product-gallery--box__image"><a href="https://blog.wacoal-america.mark4.cetxlabs.com/sample-post-with-all-8-blocks/"><img src="https://blog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/2020/12/85567_BK_PLUS.png" alt=""/></a></figure>
+ <!-- /wp:image -->
+
+ <!-- wp:paragraph {"className":"product-gallery\u002d\u002dbox__title"} -->
+ <p class="product-gallery--box__title"><a href="https://blog.wacoal-america.mark4.cetxlabs.com/sample-post-with-all-8-blocks/" target="_blank" rel="noreferrer noopener">Product Gallery</a></p>
+ <!-- /wp:paragraph -->
+
+ <!-- wp:paragraph {"className":"product-gallery\u002d\u002dbox__size"} -->
+ <p class="product-gallery--box__size">XL</p>
+ <!-- /wp:paragraph --></div>
+ <!-- /wp:column -->
+
+ <!-- wp:column {"className":"product-gallery\u002d\u002dbox"} -->
+ <div class="wp-block-column product-gallery--box"><!-- wp:image {"sizeSlug":"large","linkDestination":"custom","className":"product-gallery\u002d\u002dbox__image"} -->
+ <figure class="wp-block-image size-large product-gallery--box__image"><a href="https://blog.wacoal-america.mark4.cetxlabs.com/sample-post-with-all-8-blocks/"><img src="https://blog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/2020/12/2.png" alt=""/></a></figure>
+ <!-- /wp:image -->
+
+ <!-- wp:paragraph {"className":"product-gallery\u002d\u002dbox__title"} -->
+ <p class="product-gallery--box__title"><a href="https://blog.wacoal-america.mark4.cetxlabs.com/sample-post-with-all-8-blocks/" target="_blank" rel="noreferrer noopener">Product Gallery</a></p>
+ <!-- /wp:paragraph -->
+
+ <!-- wp:paragraph {"className":"product-gallery\u002d\u002dbox__size"} -->
+ <p class="product-gallery--box__size">XL</p>
+ <!-- /wp:paragraph --></div>
+ <!-- /wp:column -->
+
+ <!-- wp:column {"className":"product-gallery\u002d\u002dbox"} -->
+ <div class="wp-block-column product-gallery--box"><!-- wp:image {"sizeSlug":"large","linkDestination":"custom","className":"product-gallery\u002d\u002dbox__image"} -->
+ <figure class="wp-block-image size-large product-gallery--box__image"><a href="https://blog.wacoal-america.mark4.cetxlabs.com/sample-post-with-all-8-blocks/"><img src="https://blog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/2020/10/download-4-1.png" alt=""/></a></figure>
+ <!-- /wp:image -->
+
+ <!-- wp:paragraph {"className":"product-gallery\u002d\u002dbox__title"} -->
+ <p class="product-gallery--box__title"><a href="https://blog.wacoal-america.mark4.cetxlabs.com/sample-post-with-all-8-blocks/" target="_blank" rel="noreferrer noopener">Product Gallery</a></p>
+ <!-- /wp:paragraph -->
+
+ <!-- wp:paragraph {"className":"product-gallery\u002d\u002dbox__size"} -->
+ <p class="product-gallery--box__size">XL</p>
+ <!-- /wp:paragraph --></div>
+ <!-- /wp:column --></div>
+ <!-- /wp:columns --></section>
+ <!-- /wp:group -->',
+);
---
+++ themes/wacoal/includes/block-patterns/patterns/quote-timeline.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Pull Quote With Timeline
+ */
+return array(
+	'title'      => __( 'Pull Quote With Timeline ', 'wacoal' ),
+	'categories' => array( 'text' ),
+	'blockTypes' => array( 'core/template-part/text' ),
+	'content'    => '<!-- wp:group {"tagName":"section","className":"donation"} -->
+ <section class="wp-block-group donation"><!-- wp:columns {"className":"donation\u002d\u002dwrapper"} -->
+ <div class="wp-block-columns donation--wrapper"><!-- wp:column {"className":"donation\u002d\u002dwrapper-left only-timeline"} -->
+ <div class="wp-block-column donation--wrapper-left only-timeline"><!-- wp:group {"className":"timeline"} -->
+ <div class="wp-block-group timeline"><!-- wp:columns -->
+ <div class="wp-block-columns"><!-- wp:column {"className":"timeline-item"} -->
+ <div class="wp-block-column timeline-item"><!-- wp:group {"className":"timeline-text first-para"} -->
+ <div class="wp-block-group timeline-text first-para"><!-- wp:paragraph -->
+ <p><strong>Pull Quotes – TimelIne</strong></p>
+ <!-- /wp:paragraph -->
+
+ <!-- wp:paragraph -->
+ <p>In 2020, we launched Bras with a Cause and invited everyone to take part online. We donated $2 to Susan G. Komen® for every regular-price bra purchased on our website and in stores from October 4th to 10th. tetstt</p>
+ <!-- /wp:paragraph --></div>
+ <!-- /wp:group --></div>
+ <!-- /wp:column --></div>
+ <!-- /wp:columns --></div>
+ <!-- /wp:group --></div>
+ <!-- /wp:column -->
+
+ <!-- wp:column {"className":"donation\u002d\u002dwrapper-left only-timeline"} -->
+ <div class="wp-block-column donation--wrapper-left only-timeline"><!-- wp:group {"className":"timeline"} -->
+ <div class="wp-block-group timeline"><!-- wp:columns -->
+ <div class="wp-block-columns"><!-- wp:column {"className":"timeline-item"} -->
+ <div class="wp-block-column timeline-item"><!-- wp:group {"className":"timeline-text first-para"} -->
+ <div class="wp-block-group timeline-text first-para"><!-- wp:paragraph -->
+ <p>In 2020, we launched Bras with a Cause and invited everyone to take part online. We donated $2 to Susan G. Komen® for every regular-price bra purchased on our website and in stores from October 4th to 10th. tetstt</p>
+ <!-- /wp:paragraph --></div>
+ <!-- /wp:group --></div>
+ <!-- /wp:column --></div>
+ <!-- /wp:columns --></div>
+ <!-- /wp:group --></div>
+ <!-- /wp:column -->
+
+ <!-- wp:column {"className":"donation\u002d\u002dwrapper-left only-timeline"} -->
+ <div class="wp-block-column donation--wrapper-left only-timeline"><!-- wp:group {"className":"timeline"} -->
+ <div class="wp-block-group timeline"><!-- wp:columns -->
+ <div class="wp-block-columns"><!-- wp:column {"className":"timeline-item"} -->
+ <div class="wp-block-column timeline-item"><!-- wp:group {"className":"timeline-text first-para"} -->
+ <div class="wp-block-group timeline-text first-para"><!-- wp:paragraph -->
+ <p>In 2020, we launched Bras with a Cause and invited everyone to take part online. We donated $2 to Susan G. Komen® for every regular-price bra purchased on our website and in stores from October 4th to 10th. tetstt</p>
+ <!-- /wp:paragraph --></div>
+ <!-- /wp:group --></div>
+ <!-- /wp:column --></div>
+ <!-- /wp:columns --></div>
+ <!-- /wp:group --></div>
+ <!-- /wp:column -->
+
+ <!-- wp:column {"className":"donation\u002d\u002dwrapper-left only-timeline"} -->
+ <div class="wp-block-column donation--wrapper-left only-timeline"><!-- wp:group {"className":"timeline"} -->
+ <div class="wp-block-group timeline"><!-- wp:columns -->
+ <div class="wp-block-columns"><!-- wp:column {"className":"timeline-item"} -->
+ <div class="wp-block-column timeline-item"><!-- wp:group {"className":"timeline-text first-para"} -->
+ <div class="wp-block-group timeline-text first-para"><!-- wp:paragraph -->
+ <p>In 2020, we launched Bras with a Cause and invited everyone to take part online. We donated $2 to Susan G. Komen® for every regular-price bra purchased on our website and in stores from October 4th to 10th. tetstt</p>
+ <!-- /wp:paragraph --></div>
+ <!-- /wp:group --></div>
+ <!-- /wp:column --></div>
+ <!-- /wp:columns --></div>
+ <!-- /wp:group --></div>
+ <!-- /wp:column --></div>
+ <!-- /wp:columns --></section>
+ <!-- /wp:group -->',
+);
