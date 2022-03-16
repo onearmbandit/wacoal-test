932a04e2e6108b4c180660d29bcbc415f287bf9c
bhumi-cemtrex <bhpatel@cemtrexlabs.com>
2022-03-16 16:44:19 +0530

    change image gallery full bleed

--- themes/wacoal/includes/block-patterns/patterns/image-gallery-full-bleed.php
+++ themes/wacoal/includes/block-patterns/patterns/image-gallery-full-bleed.php
@@ -7,8 +7,20 @@ return array(
 	'categories' => array( 'text' ),
 	'blockTypes' => array( 'core/template-part/text' ),
 	'content'    => '<!-- wp:group {"tagName":"section","className":"full-width-slider\u002d\u002dwrapper"} -->
-	<section class="wp-block-group full-width-slider--wrapper"><!-- wp:jetpack/slideshow {"ids":[745,744,743],"sizeSlug":"full","className":"swiper-wrapper"} -->
-	<div class="wp-block-jetpack-slideshow aligncenter swiper-wrapper" data-effect="slide"><div class="wp-block-jetpack-slideshow_container swiper-container"><ul class="wp-block-jetpack-slideshow_swiper-wrapper swiper-wrapper"><li class="wp-block-jetpack-slideshow_slide swiper-slide"><figure><img alt="" class="wp-block-jetpack-slideshow_image wp-image-745" data-id="745" src="http://wacoal.local/wp-content/uploads/2022/03/Wacoal_F20_13-1.png"/></figure></li><li class="wp-block-jetpack-slideshow_slide swiper-slide"><figure><img alt="" class="wp-block-jetpack-slideshow_image wp-image-744" data-id="744" src="http://wacoal.local/wp-content/uploads/2022/03/full-width-slider-img-1.png"/></figure></li><li class="wp-block-jetpack-slideshow_slide swiper-slide"><figure><img alt="" class="wp-block-jetpack-slideshow_image wp-image-743" data-id="743" src="http://wacoal.local/wp-content/uploads/2022/03/fftc_desktop@2x.jpeg"/></figure></li></ul><a class="wp-block-jetpack-slideshow_button-prev swiper-button-prev swiper-button-white" role="button"></a><a class="wp-block-jetpack-slideshow_button-next swiper-button-next swiper-button-white" role="button"></a><a aria-label="Pause Slideshow" class="wp-block-jetpack-slideshow_button-pause" role="button"></a><div class="wp-block-jetpack-slideshow_pagination swiper-pagination swiper-pagination-white"></div></div></div>
-	<!-- /wp:jetpack/slideshow --></section>
+	<section class="wp-block-group full-width-slider--wrapper"><!-- wp:columns {"className":"swiper-container article-details-slider"} -->
+	<div class="wp-block-columns swiper-container article-details-slider"><!-- wp:column {"className":"swiper-wrapper"} -->
+	<div class="wp-block-column swiper-wrapper"><!-- wp:image {"sizeSlug":"large","className":"swiper-slide"} -->
+	<figure class="wp-block-image size-large swiper-slide"><img src="https://blog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/2021/08/Wacoal_F20_13-1.png" alt=""/></figure>
+	<!-- /wp:image -->
+
+	<!-- wp:image {"sizeSlug":"large","className":"swiper-slide"} -->
+	<figure class="wp-block-image size-large swiper-slide"><img src="https://blog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/2020/12/fftc_desktop@2x.jpeg" alt=""/></figure>
+	<!-- /wp:image -->
+
+	<!-- wp:image {"sizeSlug":"large","className":"swiper-slide"} -->
+	<figure class="wp-block-image size-large swiper-slide"><img src="https://blog.wacoal-america.mark4.cetxlabs.com/wp-content/uploads/2021/08/full-width-slider-img-1.png" alt=""/></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns --></section>
 	<!-- /wp:group -->',
 );
