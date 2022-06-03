0a3bef6f12583661cc08b5a1e7d14888f6a193de
bhumi-cemtrex <bhpatel@cemtrexlabs.com>
2022-06-03 12:55:01 +0530

    tip block bug fixes

--- themes/wacoal/template-parts/block/wacoal-tip.php
+++ themes/wacoal/template-parts/block/wacoal-tip.php
@@ -12,15 +12,17 @@
 
 ?>
 
-<?php if($tip_text && !empty($tip_text)) : ?>
+
 <section class="reminder-note  reminder-note--center">
     <div class="reminder-note--wrapper">
         <div class="content-small">
             <?php if ($tip_title && !empty($tip_title)) { ?>
                 <div class="title"><?php echo wp_kses_post($tip_title); ?></div>
             <?php } ?>
-            <div><?php echo wp_kses_post($tip_text); ?></div>
+            <?php if($tip_text && !empty($tip_text)) { ?>
+            <div class="content"><?php echo wp_kses_post($tip_text); ?></div>
+            <?php } ?>
         </div>
     </div>
 </section>
-<?php endif; ?>
+
