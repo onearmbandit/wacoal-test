c0001ec65b4aa4ff90ee82e4c9d61f82e68208da
bhumi-cemtrex <bhpatel@cemtrexlabs.com>
2022-10-14 10:52:44 +0530

    plugins updates

--- plugins/threewp-broadcast/ThreeWP_Broadcast.php
+++ plugins/threewp-broadcast/ThreeWP_Broadcast.php
@@ -7,10 +7,10 @@ Description:	Broadcast / multipost posts, with attachments, custom fields and ta
 Domain Path:	/lang
 Plugin Name:	Broadcast
 Plugin URI:		https://broadcast.plainviewplugins.com/
-Version:		48.12
+Version:		48.14
 */
 
-DEFINE( 'THREEWP_BROADCAST_VERSION', 48.12 );
+DEFINE( 'THREEWP_BROADCAST_VERSION', 48.14 );
 
 require_once( __DIR__ . '/vendor/autoload.php' );
 
--- plugins/threewp-broadcast/src/traits/terms_and_taxonomies.php
+++ plugins/threewp-broadcast/src/traits/terms_and_taxonomies.php
@@ -174,6 +174,8 @@ trait terms_and_taxonomies
 		$action->taxonomy = $taxonomy;
 		$action->execute();
 
+		$bcd->synced_taxonomies()->add( $taxonomy );
+
 		// Clean up the terms.
 		foreach( $bcd->parent_blog_taxonomies[ $taxonomy ][ 'terms' ] as $index => $term )
 			if ( ! $term )
@@ -333,8 +335,6 @@ trait terms_and_taxonomies
 			delete_option( $taxonomy . '_children' );
 			clean_term_cache( '', $taxonomy );
 		}
-
-		$bcd->synced_taxonomies()->add( $taxonomy );
 
 		// Tell everyone we've just synced this taxonomy.
 		$action = $this->new_action( 'synced_taxonomy' );
