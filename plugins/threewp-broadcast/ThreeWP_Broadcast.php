1055eea627243eb5f9998006c187e7bdf55e1233
bhumi-cemtrex <bhpatel@cemtrexlabs.com>
2023-01-06 10:57:49 +0530

    broadcast plugin update

--- plugins/threewp-broadcast/ThreeWP_Broadcast.php
+++ plugins/threewp-broadcast/ThreeWP_Broadcast.php
@@ -7,10 +7,10 @@ Description:	Broadcast / multipost posts, with attachments, custom fields and ta
 Domain Path:	/lang
 Plugin Name:	Broadcast
 Plugin URI:		https://broadcast.plainviewplugins.com/
-Version:		49.01
+Version:		49.02
 */
 
-DEFINE( 'THREEWP_BROADCAST_VERSION', 49.01 );
+DEFINE( 'THREEWP_BROADCAST_VERSION', 49.02 );
 
 require_once( __DIR__ . '/vendor/autoload.php' );
 
